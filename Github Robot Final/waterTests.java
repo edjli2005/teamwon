410cfdf24541fc799633eb75a9ab4d7b087d611d
Dumbrosie1 <sauce4mebrigham.net@gmail.com>
2014-03-18 14:43:07 -0500

    Final Code Michael Brigham
    Final Code
    Done:
    Driver(W/O GUI)
    Basic Movement
    Water Tests

--- Github Robot Final/waterTests.java
+++ Github Robot Final/waterTests.java
@@ -22,32 +22,37 @@ public class waterTests {
 		double Sdiff=100;
 		double SFIRST=100;
 		double SSECOND=-1;
-		while((Tdiff>1||Tdiff<-1)||(Sdiff>1||Sdiff<-1)){
-			//Turbidity
-			r.sleep(3000);
+		while((Tdiff>1||Tdiff<-1)||(Sdiff>1||Sdiff<-1)){//While values are still rapidly changing
+			//Turbidity									//because the sensors haven't calibrated
+			r.sleep(3000);								//to the water, keep testing the water
 			r.refreshAnalogPins();
 			r.refreshDigitalPins();
-			PIN3=r.getAnalogPin(3).getValue();
-			System.out.println("Total Light: "+PIN3);	
-			Turbidity=(Math.log(PIN3))/a;
-			System.out.println("Possible Turbidity: " + Turbidity);
-			TSECOND=TFIRST;
-			TFIRST=PIN3;
-			Tdiff=TFIRST-TSECOND;				
-				//Salinity
+			PIN3=r.getAnalogPin(3).getValue();//gets pin values
 			PIN4=r.getAnalogPin(4).getValue();
 			PIN5=r.getAnalogPin(5).getValue();
 			PIN11=r.getDigitalPin(11).getValue();
+			
+			System.out.println("Total Light: "+PIN3);//main turbidity measurement
 			R=PIN4-PIN5;
-			System.out.println("PIN4: "+PIN4);
-			System.out.println("PIN5: "+PIN5);
-			System.out.println("Current: "+R);
-			System.out.println();
+			
+			System.out.println("Current 1: "+PIN4);//main salinity measurements
+			System.out.println("Current 1: "+PIN5);
+			System.out.println("Current Diff: "+R);
+			
+			Turbidity=(Math.log(PIN3))/a;//turbidity math
+			System.out.println("Possible Turbidity: " + Turbidity);
+			
+			Salinity=(R-211.105755695)/104.3674879954;//salinity math
+			System.out.println("Possible Salinity: "+Salinity);
+			
+			TSECOND=TFIRST;
+			TFIRST=PIN3;
+			Tdiff=TFIRST-TSECOND;	//differences between measurements for turbidity				
+
 			SSECOND=SFIRST;
 			SFIRST=R;
-			Sdiff=SFIRST-SSECOND;			
-			Salinity=(R-211.105755695)/104.3674879954;
-			System.out.println("Possible Salinity: "+Salinity);
+			Sdiff=SFIRST-SSECOND;	//differences between measurements for salinity		
+		
 
 		}
 		//System.out.println(PIN4);
@@ -55,7 +60,7 @@ public class waterTests {
 		//System.out.println(PIN11);
 		//System.out.println(R);
 		//Salinity=distance/(R*area);
-		System.out.println("Final Turbidity: "+ Turbidity);
+		System.out.println("Final Turbidity: "+ Turbidity);//final outputs, will be put into array
 		System.out.println("Final Salinity: "+Salinity);
 		testValues[0]=Turbidity;
 		testValues[1]=Salinity;
