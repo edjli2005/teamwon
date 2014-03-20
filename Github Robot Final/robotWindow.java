7d0eede5a71a649b207247302f9ec43215697817
Dumbrosie1 <sauce4mebrigham.net@gmail.com>
2014-03-20 12:29:14 -0500

    Final Code Michael Brigham
    Done:
    Optimized some, added output JLabel and some color.
    Still need help with Timefield.

--- Github Robot Final/robotWindow.java
+++ Github Robot Final/robotWindow.java
@@ -26,6 +26,7 @@ public class robotWindow extends JFrame {
 		private JButton Backward;
 		private JButton Run;
 		private JTextField Timefield;
+		private JLabel Out;
 		
 		public robotWindow(final RXTXRobot r){
 		setLayout(new BorderLayout(3,2));
@@ -34,8 +35,10 @@ public class robotWindow extends JFrame {
 		sensorPanel= new JPanel();
 		movementPanel.setLayout(new FlowLayout(FlowLayout.LEFT,10,20));
 		sensorPanel.setLayout(new FlowLayout(FlowLayout.LEFT,10,20));
-		movementPanel.setPreferredSize(new Dimension(100,50));
-		sensorPanel.setPreferredSize(new Dimension(100,50));
+		movementPanel.setPreferredSize(new Dimension(200,100));
+		sensorPanel.setPreferredSize(new Dimension(200,100));
+		movementPanel.setBackground(Color.magenta);
+		sensorPanel.setBackground(Color.cyan);
 		//instantiates panels and sets their defaults
 		
 		Up=new JButton("UP");
@@ -48,9 +51,11 @@ public class robotWindow extends JFrame {
 		Salinity=new JButton("TEST SALT");
 		Turbidity=new JButton("TEST TURB.");
 		Run=new JButton("Lola's Folly");
+		Out=new JLabel("<html>Hello, my name is Lola.<br />What do you want Human?<html>");
+		
 		//instantiates buttons and gives them labels
 		
-		Timefield=new JTextField("Time?",25);
+		Timefield=new JTextField("Time?",10);
 		String Timestring = "-1";
 		final int Time=Integer.valueOf(Timestring);
 		//instantiates textfield for Time imput and converts string input
@@ -72,27 +77,29 @@ public class robotWindow extends JFrame {
 		 Up.addActionListener(new ActionListener() {//action listener for the button listens
 	         public void actionPerformed(ActionEvent e) {//for action event, when the button is pressed
 	            goUp.Up(r,Time);//and calls the appropriate method
+	            Out.setText("Up Done");
+	           
 	         }          
 	      });
 		 //repeated below for all others
 		Down.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {goDown.Down(r,Time); } });
+			public void actionPerformed(ActionEvent e) {goDown.Down(r,Time); Out.setText("Down Done"); } });
 		Left.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {turnLeft.Left(r,Time); } });
+		    public void actionPerformed(ActionEvent e) {turnLeft.Left(r,Time); Out.setText("Left Done");} });
 		Right.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {turnRight.Right(r,Time); } });
+		    public void actionPerformed(ActionEvent e) {turnRight.Right(r,Time); Out.setText("Right Done");} });
 		Forward.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {moveForward.Forward(r,Time); } });
+		    public void actionPerformed(ActionEvent e) {moveForward.Forward(r,Time); Out.setText("Forward Done");} });
 		Backward.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {moveBackward.Backward(r,Time); } });
+		    public void actionPerformed(ActionEvent e) {moveBackward.Backward(r,Time); Out.setText("Backward Done");} });
 		Water.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {testWater.waterTests(r); } });
+		    public void actionPerformed(ActionEvent e) {testWater.waterTests(r); Out.setText("Water Tests Done");} });
 		Salinity.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {testSalinity.salinityTest(r); } });
+		    public void actionPerformed(ActionEvent e) {testSalinity.salinityTest(r); Out.setText("Salt Test Done");} });
 		Turbidity.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {testTurbidity.turbidityTest(r); } });
+		    public void actionPerformed(ActionEvent e) {testTurbidity.turbidityTest(r); Out.setText("Turb. Test Done");} });
 		Run.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {LolasFolly.FULLRUN(r); } });	
+		    public void actionPerformed(ActionEvent e) {LolasFolly.FULLRUN(r); Out.setText("Lola's Folly Done?");} });	
 		
 		
 		movementPanel.add(Up);
@@ -106,6 +113,8 @@ public class robotWindow extends JFrame {
 		sensorPanel.add(Turbidity);
 		sensorPanel.add(Run);
 		sensorPanel.add(Timefield);
+		sensorPanel.add(Out);
+		
 		add(movementPanel, BorderLayout.WEST);
 		add(sensorPanel, BorderLayout.EAST);
 		//adds all our elements to the panels and the panels to the window
