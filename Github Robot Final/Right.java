f8833acb76941cd7a2bb3a4ab06828396a034ebb
skehr <shelbybriggskehr@gmail.com>
2014-03-27 18:17:19 -0500

    Update Right.java

--- Github Robot Final/Right.java
+++ Github Robot Final/Right.java
@@ -7,7 +7,7 @@ public class Right {
 		r.runMotor(RXTXRobot.MOTOR1, 125, RXTXRobot.MOTOR2, 125,Time );//starts moving
 		System.out.println("Right Done");
 		//r.sleep(1500);
-		r.close();
+		//r.close();
 	}
 
 }
