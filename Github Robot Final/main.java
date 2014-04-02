f98352f4f70aa89a431f99fc965a0008f33008fe
Dumbrosie1 <sauce4mebrigham.net@gmail.com>
2014-04-02 16:40:03 -0500

    Output file example
    output example. Sorry about the weird examples! ;op

--- Github Robot Final/main.java
+++ Github Robot Final/main.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.io.*;
 
 import javax.swing.JFrame;
 
@@ -8,12 +9,24 @@ public class main {
 
 	public static void main(String[] args) {
 		RXTXRobot r = new RXTXRobot(); // Create RXTXRobot object 
-		r.setPort("COM4"); // Set the port to COM4 
+		r.setPort("COM6"); // Set the port to COM4 
 		//r.setVerbose(true); // Turn on debugging messages 
 		r.setHasEncodedMotors(true); 
-		r.connect(); //connects to robot
+		//r.connect(); //connects to robot
 	//10 ticks is 1.5 in
 	//1 tick is .15 in
+		PrintStream Carrol = null;//new PrintStream('End of all time");
+		try {
+			Carrol = new PrintStream(new File("output.txt"));
+			//System.out.println("Ryan Hardy is meaningless to Carrol??!!");
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("OMFG CLAIRE'S ALIVE!!!!");
+		}
+		System.setOut(Carrol);
+		//System.out.println("Why not Poe?");
+		//Carrol.println("Ryan Hardy. The one cop who refuses to carry a tazer. Despite the hordes of crazies with knives.");
 		
 		robotWindow window= new robotWindow(r);
 		window.setSize(600,175);
