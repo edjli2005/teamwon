a7ce4a77174e621289f6fa0565ce20cfe6677c67
Mdzingo <morg.monzingo@gmail.com>
2014-04-16 04:22:01 -0500

    just changed salinity (I hope)

---
+++ GithubRobotFinal/salinityTest.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import rxtxrobot.*;
+
+
+public class salinityTest {
+
+	public salinityTest(){}//constructor needed for ActionListener
+	public void salinityTest(RXTXRobot r) {
+		//RXTXRobot r = new RXTXRobot(); // Create RXTXRobot object 
+		Scanner s= new Scanner(System.in);
+		
+		
+		r.refreshAnalogPins();
+	
+			r.refreshAnalogPins();
+			r.refreshDigitalPins();
+			int conductivity=0;
+		
+			for ( int x =0; x<=10; x++)
+			{
+				
+			conductivity = r.getConductivity();
+			}
+			
+			
+			
+			double testedConductivity = (-0.30102*conductivity*conductivity) + (99.05*conductivity) - 6822.9;
+			System.out.println("Conductivity: " + testedConductivity);
+	}
+
+}
